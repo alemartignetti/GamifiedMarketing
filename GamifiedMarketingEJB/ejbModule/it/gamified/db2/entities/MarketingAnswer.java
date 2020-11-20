15ab2f0db06bf2660eb3bbbb3d0cf0173ce04e5d
KingPowa <KingPowa@DESKTOP-BODVQLJ>
2020-11-20 21:54:36 +0100

    owo

--- GamifiedMarketingEJB/ejbModule/it/gamified/db2/entities/MarketingAnswer.java
+++ GamifiedMarketingEJB/ejbModule/it/gamified/db2/entities/MarketingAnswer.java
@@ -1,5 +1,25 @@
 package it.gamified.db2.entities;
 
-public class MarketingAnswer {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="marketinganswer", schema="db_gamifiedschema")
+public class MarketingAnswer implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+	
+	public enum Type {
+	    LOGIN,
+	    CANCEL;
+	}
+	
+	@Column(columnDefinition = "ENUM('LOGIN', 'CANCEL')")
+	@Enumerated(EnumType.STRING)
+	private Type type;
 }
+
