c0f33bc5ce0d16e2d77d0d70001c8f130c0d7571
KingPowa <kingpowaalias@gmail.com>
2020-11-28 00:54:31 +0100

    owo

--- GamifiedMarketingEJB/ejbModule/it/gamified/db2/exceptions/NoDailyQuestionnaire.java
+++
@@ -1,9 +0,0 @@
-package it.gamified.db2.exceptions;
-
-public class NoDailyQuestionnaire extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public NoDailyQuestionnaire(String message) {
-		super(message);
-	}
-}
